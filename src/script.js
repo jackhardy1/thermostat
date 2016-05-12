a1874ac9f3c07c18f71bac627fbdf5eb52205edf
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-12 16:00:39 +0100

    Used JQuery to link buttons to thermometer methods

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  var thermostat = new Thermostat();
+
   $('button').mouseenter(function(){
     $(this).fadeTo('fast',0.25);
   });
@@ -6,5 +8,31 @@ $(document).ready(function(){
     $(this).fadeTo('slow',1);
   });
 
-  $('#temperature')
+  $('#temperature').text(thermostat.getTemperature());
+
+  $('#up').click(function() {
+    thermostat.increaseTemperature();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+
+  $('#down').click(function() {
+    thermostat.decreaseTemperature();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+
+  $('#powerSaveOn').click(function() {
+    thermostat.powerSaveOn();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+
+  $('#powerSaveOff').click(function() {
+    thermostat.powerSaveOff();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+
+  $('#reset').click(function() {
+    thermostat.resetTemp();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+
 });
