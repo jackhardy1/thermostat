0c99492f9a7f6a88979330ae561ee15025a2f07f
Jack Hardy <jackhardy1@gmail.com>
2016-05-13 15:56:18 +0100

    added css

--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
-$.ajax({
-  url: 'api.openweathermap.org/data/2.5/weather?'
-});
 
 $(document).ready(function(){
   var thermostat = new Thermostat();
   updateTemp();
 
+  $('#city').change(function(){
+    var city = $('#city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=9ecba4652a14069c1e2d09e14d2c93c9&units=metric', function(data) {
+    $('#temp').text(data.main.temp);
+    $('#sea-level').text(data.main.sea_level);
+    $('#wind-speed').text(data.wind.speed);
+  });
+});
+
   $('button').mouseenter(function(){
     $(this).fadeTo('fast',0.25);
   });
-  
+
   $('button').mouseleave(function(){
     $(this).fadeTo('slow',1);
   });
